87ad87bc25055ba119b0281e805ae7e049fab69f
LeoFdz <lfernandez79@gmail.com>
2020-02-28 20:32:42 -0600

    added insert method

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,22 @@
 var orm = {
-    selectAll: function(tableInput, colToSearch, valOfCol, cb) {
-        var queryStrg = "SELECT * FROM ?? WHERE ?? = ?";
-        RTCPeerConnection.query(queryStrg, [tableInput, colToSearch, valOfCol], function(err, result){
-            if (err) throw err;
+    selectAll: function(tableInput, cb) {
+        var queryStrg = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryStrg, function(err, result){
+            if (err) {
+                throw err;
+            }
             cb(result)
-        })
-    }
+        });
+    },
 
-    
-};
+    insertOne: function(whereToInsert, insertInto, insertValue) {
+        var queryInsert = "INSERT INTO burgers (burger_name) VALUES (?)";
+        connection.query(queryInsert, [whereToInsert, insertInto, insertValue,] function(err, result) {
+          if (err) {
+              throw err;
+          } 
+    });
+  },
 
+}
 module.exports = orm;
